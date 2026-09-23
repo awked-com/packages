82bf385ee9eef1d0f3b4348c51b119998f9718da
Rany Hany <rany_hany@riseup.net>
2026-09-23 06:29:35 +0300

    nix-ci-worker: Fix empty cache admission

--- pkgs/nix-ci-worker/default.nix
+++ pkgs/nix-ci-worker/default.nix
@@ -10,8 +10,8 @@ buildGoModule {
   src = fetchFromGitHub {
     owner = "awked-com";
     repo = "nix-ci-worker";
-    rev = "53421694be46fc47377b5d7031c2e67bfda95a0d";
-    hash = "sha256-Sntc4kALiNLVOfa0/1RD+DfHe5N8FxRWKqU0vy4N7aU=";
+    rev = "2629356170fb86c0125651b5a6a8216a38daad9c";
+    hash = "sha256-5sYFSdpE0AHC3J1QGQrppTFj7+Q9yHN/n8b3kFsZZ7w=";
   };
 
   vendorHash = "sha256-RlhQtMIS/spLuTbgzXPG0t+ouNCAfy5feP1btkFC5+g=";
